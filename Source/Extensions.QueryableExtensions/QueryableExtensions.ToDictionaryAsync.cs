3e23da44352f1686cc6a0b017e290cc394483523
Scott Keith Thomas, Jr <sthomas@idis.com>
2016-06-30 13:15:42 -0600

    Reformatting class structure.

--- Source/Extensions.QueryableExtensions/QueryableExtensions.ToDictionaryAsync.cs
+++ Source/Extensions.QueryableExtensions/QueryableExtensions.ToDictionaryAsync.cs
@@ -6,9 +6,6 @@ using System.Threading.Tasks;
 
 namespace Extensions.QueryableExtensions
 {
-    /// <summary>
-    ///     Extension methods for use with <see cref="IQueryable{T}" />.
-    /// </summary>
     public static partial class QueryableExtensions
     {
         /// <summary>
---
+++ Source/Extensions.QueryableExtensions/QueryableExtensions.cs
@@ -0,0 +1,11 @@
+﻿using System.Linq;
+
+namespace Extensions.QueryableExtensions
+{
+    /// <summary>
+    ///     Extension methods for use with <see cref="IQueryable{T}" />.
+    /// </summary>
+    public static partial class QueryableExtensions
+    {
+    }
+}
